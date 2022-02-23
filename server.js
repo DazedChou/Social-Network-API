78568a56241fc1a67c2ba7dd6d751bdc1aa5789c
David Chou <davidchou2009@gmail.com>
2022-02-22 20:31:04 -0800

    import required modules and equip server to listen for requests on server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const db = require('./config/connection');
+// Require model
+const { User, Reaction, Thought } = require('./models');
+
+const routes = require('./controllers');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(routes);
+
+db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+    });
+  });
+  
