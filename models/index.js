2251ba5b82ac612428955af8feebf8c0af61d0b9
David Chou <davidchou2009@gmail.com>
2022-02-22 22:07:51 -0800

    create files for api routes

--- models/index.js
+++ models/index.js
@@ -1,3 +1,5 @@
 const User = require('./User');
+const Thought = require('./Thought');
+const Reaction = require('./Reaction');
 
-module.exports = { User };
+module.exports = { User, Reaction, Thought };
