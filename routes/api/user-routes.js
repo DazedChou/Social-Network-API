1c35219de92abb2fd121b1a684fe011f16b8f622
David Chou <davidchou2009@gmail.com>
2022-02-23 17:04:58 -0800

    setup user routes

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,3 +1,11 @@
 const router = require('express').Router();
 const {User, Thought, Reaction} = require('../../models');
 
+// /api/users
+router.route('/').get(getUsers).post(createUser);
+
+// /api/users/:userId
+router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
+
+// /api/users/:userId/friends/:friendId
+router.route('/:userId/friends/:friendId').post(createFriend).delete(deleteFriend);
