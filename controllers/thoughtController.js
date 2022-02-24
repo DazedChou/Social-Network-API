0b90ec22c26721f82ac2243223ef34639adb4c83
David Chou <davidchou2009@gmail.com>
2022-02-23 21:16:49 -0800

    create controller route to retreive all and single thoughts, and to create a new thought

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -8,5 +8,21 @@ module.exports = {
                 console.error({ message: err });
                 return res.status(500).json(err);
             });
+    }, 
+    getSingleThought(req, res) {
+        Thought.findOne({ _id: req.params.thoughtId })
+            .then((thought) =>
+                !thought
+                    ? res.status(404).json({ message: "No thought with that ID" })
+                    : res.json(thought)
+            )
+    },
+    createThought(req, res) {
+        Thought.create(req.body)
+        .then((thought) => res.json(thought))
+        .catch((err) => {
+            console.error({ message: err });
+            return res.status(500).json(err);
+        });
     }
 }
