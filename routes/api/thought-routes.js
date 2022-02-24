c374d9004a2129c06d0d36445abe5193b342e44b
David Chou <davidchou2009@gmail.com>
2022-02-23 20:34:25 -0800

    create api routes for thoughts

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+const { getThoughts, createThought, getSingleThought, updateThought, deleteThought, createReaction, deleteReaction} = require('../../controllers/userController');
+
+// /api/users
+router.route('/').get(getThoughts).post(createThought);
+
+// /api/users/:userId
+router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
+
+// /api/thoughts/:thoughtId/reactions
+router.route('/:thoughtId/reactions').post(createReaction).delete(deleteReaction);
