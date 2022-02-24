bea5923a024341ecb5d8fb9a2541d0cae32aec16
David Chou <davidchou2009@gmail.com>
2022-02-23 16:54:55 -0800

    corrected folder name from controllers to routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 const userRoutes = require ('./user-routes');
 const thoughtRoutes = require ('./thought-routes');
 
-router.use('/user', userRoutes);
+router.use('/users', userRoutes);
 router.use('/thoughts', thoughtRoutes);
 
 module.exports = router;
